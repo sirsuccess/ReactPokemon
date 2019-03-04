aa8d08c2c6cd35063684b6ea8c14d0f45130cd8e
Amani Kanu <kanuamani@gmail.com>
2019-03-04 14:58:39 +0100

    list changes

--- src/App.js
+++ src/App.js
@@ -3,42 +3,43 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  state ={
+  state = {
     card: []
 
   };
 
-  
+
   componentDidMount() {
 
     fetch('https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json')
-        .then(response => response.json())
-        .then(apiData => {
-          let pokData = apiData.pokemon;
-          this.setState({
-               card : pokData,
-          })
-          // .catch(err => console.log("Oops Error: " + err))
-    
-})}
- 
+      .then(response => response.json())
+      .then(apiData => {
+        let pokData = apiData.pokemon;
+        this.setState({
+          card: pokData,
+        })
+        // .catch(err => console.log("Oops Error: " + err))
+
+      })
+  }
+
   render() {
     return (
-          < div className="list">
+      < div className="list">
 
-          <h1 className="header">POKEMON APP</h1>
-          <div className="card">
-          <ul>
-    {this.state.card.map(card => <li key={card.id}> <><img src={card.img} alt="Pokemon" /> <p>{card.name}</p></></li>)}
-          </ul>                  
-          </div>
+        <h1 className="header">POKEMON APP</h1>
+        <div className="card">
+        <ul className="list">
+            {this.state.card.map(card => <li key={card.id}> <><img src={card.img} alt="Pokemon" /> <p>{card.name}</p></></li>)}
+          </ul>
+        </div>
 
-            
-          </div>
-       
-    
+
+      </div>
+
+
     );
-        }
+  }
 }
 
 export default App;
